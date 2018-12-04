d773847e70ec4602331939778ad750bd5efc16cf
ptarnaud <patrick.tarnaud@systeme-u.fr>
2018-12-04 16:47:14 +0100

    Feature/find events with parameters (#4)
    * init resource and findEvents added

--- business/EventsLBS.js
+++ business/EventsLBS.js
@@ -1,7 +1,8 @@
 import * as eventsDAO from '../data/EventsDAO'
 
-export const findEvents = async () => {
-  return await eventsDAO.findEvents()
+export const findEvents = async (groupId, typeGroup, beginDate, endDate, interestedId, administratorId, participantId) => {
+  console.log("LBS participant" , participantId)
+  return await eventsDAO.findEvents(groupId, typeGroup, beginDate, endDate, interestedId, administratorId, participantId)
 }
 
 export const getEvent = async eventId => {
@@ -10,4 +11,8 @@ export const getEvent = async eventId => {
 
 export const createEvent = async event => {
   return await eventsDAO.createEvent(event)
+}
+
+export const init = async () => {
+  return await eventsDAO.init()
 }
--- exposition/EventsEBS.js
+++ exposition/EventsEBS.js
@@ -1,12 +1,24 @@
 import express from 'express'
 import * as eventsLBS from '../business/EventsLBS'
 
-export const  getRouter = () => {
+export const getRouter = () => {
   let eventsRouter = express.Router()
 
   eventsRouter.get('/', async (req, res) => {
     try {
-      res.send(await eventsLBS.findEvents())
+      //    for (const key in req.query) {
+      //      console.log(key, req.query[key])
+      //    }
+      console.log(req.query.groupId)
+      console.log(req.query.typeGroup)
+      console.log(req.query.beginDate)
+      console.log(req.query.endDate)
+      console.log(req.query.idUser)
+      console.log(req.query.interestedId)
+      console.log(req.query.administratorId)
+      console.log(req.query.participantId)
+
+      res.send(await eventsLBS.findEvents(req.query.groupId, req.query.typeGroup, req.query.beginDate, req.query.endDate, req.query.interestedId, req.query.administratorId, req.query.participantId))
     } catch (error) {
       console.log('An error occured', error)
       res.status(500).send('An error occured')
@@ -28,6 +40,15 @@ export const  getRouter = () => {
     } catch (error) {
       console.log('An error occured', error)
       res.status(500).send('An error occured')
+    }
+  })
+
+  eventsRouter.post('/init', async(req, res) => {
+    try {
+      res.send(await eventsLBS.init())
+    } catch (error) {
+      console.log(error)
+      
     }
   })
 
