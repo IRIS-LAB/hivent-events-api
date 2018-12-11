8627192333f2ecf054ec698ec91d8d58a66efc8d
jgoizil <jgoizil@gmail.com>
2018-12-11 16:36:03 +0100

    Feature/find (#5)
    * find done

--- data/EventsDAO.js
+++ data/EventsDAO.js
@@ -1,7 +1,7 @@
 import { MongoClient, ObjectId } from 'mongodb'
-import {eventsDB} from './init/EventsDB'
+import { eventsDB } from './init/EventsDB'
 
-const url = 'mongodb://localhost:27017/'
+const url = 'mongodb://localhost:27021/'
 
 const connect = async () => {
 	let connection = await MongoClient.connect(
@@ -21,13 +21,45 @@ export const findEvents = async (
 	administratorId,
 	participantId
 ) => {
-  let eventsDB = await connect()
-  console.log("participant :",participantId)
-  console.log(typeof(participantId))
+	let eventsDB = await connect()
+	let findRequest = {}
+	if (groupId !== undefined) {
+		findRequest.groupId = parseInt(groupId)
+	}
+	if (groupId !== undefined) {
+		findRequest.groupId = parseInt(groupId)
+	}
+	if (beginDate !== undefined) {
+		findRequest.startDate = {}
+		findRequest.startDate.$gte = new Date(beginDate)
+	}
+	if (endDate !== undefined) {
+		if (!findRequest.startDate) {
+			findRequest.startDate = {}
+		}
+		findRequest.startDate.$lte = new Date(endDate)
+	}
+	if (interestedId !== undefined) {
+		if (!findRequest.interested) {
+			findRequest.interested = {}
+		}
+		findRequest.interested.id = parseInt(interestedId)
+	}
+	if (administratorId !== undefined) {
+		if (!findRequest.administrators) {
+			findRequest.administrators = {}
+		}
+		findRequest.administrators.id = parseInt(administratorId)
+	}
+	if (participantId !== undefined) {
+		if (!findRequest.participants) {
+			findRequest.participants = {}
+		}
+		findRequest.participants.id = parseInt(participantId)
+	}
 	let events = await eventsDB
 		.collection('Events')
-		.find({"participants.id": parseInt(participantId)}).toArray()
-  console.log(events)
+		.find(findRequest).toArray()
 	// eventsDB.connection.close()
 	return events
 }
@@ -48,8 +80,8 @@ export const createEvent = async event => {
 }
 
 export const init = async () => {
-  let db = await connect()
-  //await db.collection('Events').drop()
+	let db = await connect()
+	//await db.collection('Events').drop()
 	let newEvents = await db.collection('Events').insertMany(eventsDB)
 	return newEvents.ops
 }
