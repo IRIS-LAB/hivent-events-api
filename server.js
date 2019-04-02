5ed62aba477d9f85b3aad2f2ae94d385b5d9b853
Mathilde Scebalt <mathilde.scebalt@systeme-u.fr>
2019-04-02 17:10:23 +0200

    add cors

--- server.js
+++ server.js
@@ -2,12 +2,12 @@ import express from 'express'
 import bodyParser from 'body-parser'
 import * as eventsEBS from './src/exposition/EventsEBS'
 import * as actuatorEBS from './src/exposition/ActuatorEBS'
-import {Logger, expressUtils} from '@u-iris/iris-back'
+import { Logger, expressUtils } from '@u-iris/iris-back'
 import dotenv from 'dotenv'
 
 dotenv.config()
 const app = express()
-const port = process.env.PORT || 8080
+const port = process.env.PORT || 8081
 
 const logger = Logger.create(process.env.LOGLEVEL, process.env.LOGFILENAME)
 
@@ -17,6 +17,7 @@ app.listen(port, () => {
 
 const utils = expressUtils(logger)
 // Pour récupérer body des requêtes
+app.use(utils.enableCors)
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
